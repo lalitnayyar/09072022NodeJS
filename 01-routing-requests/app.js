76292054084ff347d5309380a6152d5b7ab15c8d
Lalit <lalitnayyar@gmail.com>
2022-07-10 12:40:49 +0400

    test

--- 01-routing-requests/app.js
+++ 01-routing-requests/app.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', text);
+    fs.writeFileSync('message.txt', 'Dummy');
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
